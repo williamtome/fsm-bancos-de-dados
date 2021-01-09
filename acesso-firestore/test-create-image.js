0a6446fd49f56373c10c3615ba644f1586c18aa6
William Tomé <william.tomenh@gmail.com>
2021-01-09 12:30:56 -0300

    criar imagens

---
+++ acesso-firestore/test-create-image.js
@@ -0,0 +1,23 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./firebase.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore()
+
+const productId = 'D3zbkoXSTa6T8iL3H9n3'
+
+const imageRef = db
+    .collection('products')
+    .doc(productId)
+    .collection('images')
+    .doc()
+
+imageRef
+    .set({
+        description: 'my description',
+        url: 'my image url'
+    }).then(res => console.log(res))
