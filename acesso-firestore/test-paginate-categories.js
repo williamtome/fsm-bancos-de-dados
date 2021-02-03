ddc5c44e713a44e43f3a2333b7b6c2b47aada352
William Tomé <william.tomenh@gmail.com>
2021-02-03 00:02:47 -0300

    Aprendendo a implementar a lógica de paginação para banco nosql.

---
+++ acesso-firestore/test-paginate-categories.js
@@ -0,0 +1,33 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./firebase.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore()
+
+const pageSize = 1
+
+const categories = db
+                    .collection('categories')
+                    .orderBy('category')
+                    .limit(pageSize+1)
+                    .startAfter('Eletrônicos')
+                    .get()
+                    
+categories.then(snapshot => {
+  let total = 0
+  snapshot.forEach(doc => {
+    if (total < pageSize) {
+      console.log('ID: ' + doc.id, ' => ', doc.data().category);
+    } 
+    total++
+  })
+  if (total > pageSize) {
+    console.log('has next page.');
+  } else {
+    console.log('doesnt have next page.');
+  }
+})
