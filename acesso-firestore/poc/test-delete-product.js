eead6e4aa5810d90faa0070dc71bacdfca20ad27
William Tomé <william.tomenh@gmail.com>
2021-03-01 22:26:21 -0300

    corrigido identação.

--- acesso-firestore/poc/test-delete-product.js
+++ acesso-firestore/poc/test-delete-product.js
@@ -11,22 +11,22 @@ const db = admin.firestore()
 const productId = 'D3zbkoXSTa6T8iL3H9n3'
 const product = db.collection('products').doc(productId)
 
-    db.collection('products')
-      .doc(productId)
-      .collection('images')
-      .get()
-      .then(imgSnapshot => {
-        const exclusoes = []
-        imgSnapshot.forEach(img => {
-          console.log(img.id);
-          exclusoes.push(db.collection('products').doc(productId).collection('images').doc(img.id).delete())
-        })
-        return Promise.all(exclusoes)
-      })
-      .then(() => {
-        console.log('Imagens removidas com sucesso.');
-        return product.delete()
-      })
-      .then(() => {
-        console.log('Produto removido com sucesso.');
-      })
+db.collection('products')
+  .doc(productId)
+  .collection('images')
+  .get()
+  .then(imgSnapshot => {
+    const exclusoes = []
+    imgSnapshot.forEach(img => {
+      console.log(img.id);
+      exclusoes.push(db.collection('products').doc(productId).collection('images').doc(img.id).delete())
+    })
+    return Promise.all(exclusoes)
+  })
+  .then(() => {
+    console.log('Imagens removidas com sucesso.');
+    return product.delete()
+  })
+  .then(() => {
+    console.log('Produto removido com sucesso.');
+  })
