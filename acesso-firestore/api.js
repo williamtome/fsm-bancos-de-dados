f68ff9f151cccf27656baa8790c687f72b0f6989
William Tomé <william.tomenh@gmail.com>
2021-03-01 23:41:41 -0300

    testar métodos

--- acesso-firestore/api.js
+++ acesso-firestore/api.js
@@ -1,4 +1,5 @@
 const categories = require('./categories');
+const products = require('./products');
 
 const testes = async() => {
   // await categories.create({
@@ -12,9 +13,29 @@ const testes = async() => {
   // await categories.update('8V2v7jqEd5XpUi6eCojn', {
   //   category: 'Novo nome da categoria'
   // })
+  // const cat = await categories.findAll()
 
-  const cat = await categories.findAllPaginate({})
-  console.log(cat);
+  // const cat = await categories.findAllPaginate({})
+  // console.log(cat);
+
+/*  await products.create({
+    product: 'Novo produto2',
+    price: 797,
+    categories: ['8V2v7jqEd5XpUi6eCojn']
+  })*/
+  
+  /*await products.update('8am8ykeuv3qxFygZXTZx', {
+    product: 'Novo produto2',
+    price: 797,
+    categories: ['duiQuEoh4JHJtPXPnQLt']
+  })*/
+  //products.remove('4bKsa1HMIAfT7gFb16su')
+
+  // await products.addImage('UN1qS1knXSGx2IczbQWT', {description: 'Nova imagem', url: 'url'})
+  const prods = await products.findAllPaginate({pageSize: 1, startAfter: ''})
+  // const prods = await products.findAll()
+  console.log(prods);
+
 }
 
 testes()
