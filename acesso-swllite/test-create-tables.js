508d775a22e2fe6f727bb4a37c891e341254a615
William Tomé <william.tomenh@gmail.com>
2021-03-03 21:42:22 -0300

    Criado queries de criação das tabelas para o banco.

---
+++ acesso-swllite/test-create-tables.js
@@ -0,0 +1,55 @@
+const sqlite = require('sqlite3').verbose()
+
+const initDB = databaseFile => new Promise((resolve, reject) => {
+  const db = new sqlite.Database(databaseFile, (err) => {
+    if (err) {
+      reject(err)
+    }else{
+      resolve(db)
+    }
+  })
+})
+
+const run = (db, query) => new Promise((resolve, reject) => {
+  db.run(query, err => {
+    if (err) {
+      reject(err)
+    }else{
+      resolve()
+    }
+  })
+})
+
+const createTables = async() => {
+  const db = await initDB('banquinho.sqlite3')
+  await run(db, `
+    CREATE TABLE categories (
+      id INTEGER NOT NULL PRIMARY KEY,
+      category TEXT
+    );
+  `)
+  await run(db, `
+    CREATE TABLE products (
+      id INTEGER NOT NULL PRIMARY KEY,
+      product TEXT,
+      price REAL
+    );
+  `)
+  await run(db, `
+    CREATE TABLE images (
+      id INTEGER NOT NULL PRIMARY KEY,
+      description TEXT,
+      url TEXT,
+      product_id INTEGER REFERENCES products(id)
+    );
+  `)
+  await run(db, `
+    CREATE TABLE categories_products (
+      category_id INTEGER REFERENCES categories(id),
+      product_id INTEGER REFERENCES products(id)
+    );
+  `)
+  console.log('Tables were created!');
+}
+
+createTables().catch(err => console.log(err))
