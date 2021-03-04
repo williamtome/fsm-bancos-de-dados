0117721040c2ae71af8935aeec2d3df97baae097
William Tomé <william.tomenh@gmail.com>
2021-03-04 19:17:08 -0300

    Atualizar categoria.

---
+++ acesso-sqllite/test-update-categories.js
@@ -0,0 +1,29 @@
+const sqlite = require('sqlite3').verbose()
+
+const initDB = databaseFile => new Promise((resolve, reject) => {
+  const db = new sqlite.Database(databaseFile, (err) => {
+    if (err) {
+      reject(err)
+    }else{
+      resolve(db)
+    }
+  })
+})
+
+const run = (db, query, values) => new Promise((resolve, reject) => {
+  db.run(query, values, err => {
+    if (err) {
+      reject(err)
+    }else{
+      resolve()
+    }
+  })
+})
+
+const createCategories = async() => {
+  const db = await initDB('banquinho.sqlite3')
+  await run(db, `UPDATE categories SET category = ? WHERE id = ?;`, ['Atualizado categoria 1', 1])
+  console.log('Category updated!');
+}
+
+createCategories().catch(err => console.log(err))
