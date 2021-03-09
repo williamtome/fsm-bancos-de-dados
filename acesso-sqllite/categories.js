e0da9b4525c46ec93698b915a20f72b9c69170df
William Tomé <william.tomenh@gmail.com>
2021-03-09 19:04:12 -0300

    Criado listagem das categorias com paginação dos dados.

--- acesso-sqllite/categories.js
+++ acesso-sqllite/categories.js
@@ -24,9 +24,22 @@ const update = async(data, id) => {
   console.log('Category updated!');
 }
 
+const findAllPaginate = async({ pageSize = 1, currentPage = 0 }) => {
+  const dbConn = await db.init('./banquinho.sqlite3')
+  const categories = await db.query(dbConn, `SELECT * FROM categories limit ${currentPage*pageSize}, ${pageSize+1};`)
+  
+  if (categories.length > pageSize) 
+    categories.pop()
+  
+  return {
+    data: categories,
+    hasNext: categories.length > pageSize
+  }
+}
+
 module.exports = {
   findAll,
-  // findAllPaginate,
+  findAllPaginate,
   create,
   update,
   remove
