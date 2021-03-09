37740ab51f04005c423e1a17f590a5fcaa675607
William Tomé <william.tomenh@gmail.com>
2021-03-09 19:03:43 -0300

    Todas as funcionalidades criadas.

--- acesso-sqllite/api.js
+++ acesso-sqllite/api.js
@@ -1,10 +1,13 @@
 const categories = require('./categories')
 
 const test = async() => {
-  // categories.create('Decoração')
-  // categories.remove(1)
-  categories.update('Informática', 5)
-  // categories.findAll()
+  await categories.findAll();
+  await categories.create('Decoração')
+  await categories.remove(1)
+  await categories.update('Informática', 5)
+  console.log('cp 0:',await categories.findAllPaginate({ pageSize: 2, currentPage: 0 }));
+  console.log('cp 1:',await categories.findAllPaginate({ pageSize: 2, currentPage: 1 }));
+  console.log('cp 2:',await categories.findAllPaginate({ pageSize: 2, currentPage: 2 }));
 }
 
 test().catch(err => console.log(err))
