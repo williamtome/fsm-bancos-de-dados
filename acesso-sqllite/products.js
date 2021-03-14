9ae7a5f659a702e9ebdd09f21f954bd9f1b07070
William Tomé <william.tomenh@gmail.com>
2021-03-14 16:50:51 -0300

    Crud de produtos.

---
+++ acesso-sqllite/products.js
@@ -0,0 +1,102 @@
+const db = require('./db')
+
+const init = database => {
+
+  const create = async(data) => {
+    const dbConn = await db.init(database)
+    await db.queryWithParams(dbConn, `INSERT INTO products (product, price) VALUES (?, ?);`, data)
+    console.log('product created!');
+  }
+  
+  const addImage = async(data, productId) => {
+    const dbConn = await db.init(database)
+    await db.queryWithParams(dbConn, `INSERT INTO images (description, url, product_id) VALUES (?, ?, ?)`, [...data, productId])
+    console.log('Image for product created!');
+  }
+
+  const findAll = async() => {
+    const dbConn = await db.init(database)
+    // const products = await db.query(dbConn, `SELECT * FROM products LEFT JOIN images ON products.id = images.product_id group by images.product_id;`)
+    const products = await db.query(dbConn,'SELECT * FROM products')
+    const condition = products.map(product => product.id).join(', ')
+    const images = await db.query(dbConn,'select * from images where product_id in ('+condition+') GROUP BY product_id')
+    const mapImages = images.reduce((prev, current) => {
+      return {
+        ...prev,
+        [current.product_id]: current
+      }
+    }, {})
+
+    const productsWithImages = products.map(product => {
+      return {
+        ...product,
+        image: mapImages[product.id]
+      }
+    })
+    console.log('products:', productsWithImages);
+  }
+
+  const findAllImages = async() => {
+    const dbConn = await db.init(database)
+    const images = await db.query(dbConn, `SELECT * FROM images;`)
+    console.log('images:', images);
+  }
+  
+  const remove = async(id) => {
+    const dbConn = await db.init(database)
+    await db.queryWithParams(dbConn, `DELETE FROM products WHERE id = ?;`, id)
+    await db.queryWithParams(dbConn, `DELETE FROM images WHERE product_id = ?;`, id)
+    await db.queryWithParams(dbConn, `DELETE FROM categories_products WHERE product_id = ?;`, id)
+    console.log('product DELETED!');
+  }
+  
+  const update = async(data, id) => {
+    const dbConn = await db.init(database)
+    await db.queryWithParams(dbConn, `UPDATE products SET product = ?, price = ? WHERE id = ?;`, [...data, id])
+    console.log('product updated!');
+  }
+  
+  const findAllPaginate = async({ pageSize = 1, currentPage = 0 }) => {
+    const dbConn = await db.init(database)
+    const products = await db.query(dbConn, `SELECT * FROM products limit ${currentPage*pageSize}, ${pageSize+1};`)
+    const hasNext = products.length > pageSize
+
+    if (products.length > pageSize) 
+      products.pop()
+
+    const condition = products.map(product => product.id).join(', ')
+    const images = await db.query(dbConn,'select * from images where product_id in ('+condition+') GROUP BY product_id')
+    const mapImages = images.reduce((prev, current) => {
+      return {
+        ...prev,
+        [current.product_id]: current
+      }
+    }, {})
+
+    const productsWithImages = products.map(product => {
+      return {
+        ...product,
+        image: mapImages[product.id]
+      }
+    })
+
+    
+    return {
+      data: productsWithImages,
+      hasNext
+    }
+  }
+
+  return {
+    findAll,
+    findAllPaginate,
+    findAllImages,
+    create,
+    addImage,
+    update,
+    remove
+  }
+
+}
+
+module.exports = init
