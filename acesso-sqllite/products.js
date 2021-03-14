272450a8bd37bdaf9b729bc9ecf9f662c6893558
William Tomé <william.tomenh@gmail.com>
2021-03-14 20:05:04 -0300

    Função para atualizar tabela categories_products.

--- acesso-sqllite/products.js
+++ acesso-sqllite/products.js
@@ -16,7 +16,6 @@ const init = database => {
 
   const findAll = async() => {
     const dbConn = await db.init(database)
-    // const products = await db.query(dbConn, `SELECT * FROM products LEFT JOIN images ON products.id = images.product_id group by images.product_id;`)
     const products = await db.query(dbConn,'SELECT * FROM products')
     const condition = products.map(product => product.id).join(', ')
     const images = await db.query(dbConn,'select * from images where product_id in ('+condition+') GROUP BY product_id')
@@ -54,6 +53,13 @@ const init = database => {
     const dbConn = await db.init(database)
     await db.queryWithParams(dbConn, `UPDATE products SET product = ?, price = ? WHERE id = ?;`, [...data, id])
     console.log('product updated!');
+  }
+
+  const updateCategories = async(id, categories) => {
+    const dbConn = await db.init(database)
+    await db.queryWithParams(dbConn, `DELETE FROM categories_products WHERE product_id = ?;`, id)
+    for await(const category of categories)
+      await db.queryWithParams(dbConn, `INSERT INTO categories_products (category_id, product_id) VALUES (?, ?);`, [category, id])
   }
   
   const findAllPaginate = async({ pageSize = 1, currentPage = 0 }) => {
@@ -94,6 +100,7 @@ const init = database => {
     create,
     addImage,
     update,
+    updateCategories,
     remove
   }
 
