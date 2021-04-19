b3046c58316b4c506521bbb721ccfa7a68531944
William Tomé <william.tomenh@gmail.com>
2021-04-18 21:57:19 -0300

    Chamada dos métodos de categoria.

--- acesso-mysql/api.js
+++ acesso-mysql/api.js
@@ -3,6 +3,9 @@ const categories = require('./categories')(connection)
 
 const test = async() => {
   console.log(await categories.findAll())
+  // await categories.create('Smartphones')
+  // await categories.remove(2)
+  // console.log(await categories.update('Eletrônicos & Eletrodomésticos', 1))
 }
 
 test()
