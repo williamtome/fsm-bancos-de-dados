9c7a79fa1c64b4bcb418568e9d8cbb6b0927d908
William Tomé <william.tomenh@gmail.com>
2021-04-19 19:26:52 -0300

    ajuste.

--- acesso-mysql/api.js
+++ acesso-mysql/api.js
@@ -1,11 +1,15 @@
 const connection = require('./db')
 const categories = require('./categories')(connection)
+const products = require('./products')(connection)
 
 const test = async() => {
-  console.log(await categories.findAll())
+  // console.log(await categories.findAll())
   // await categories.create('Smartphones')
   // await categories.remove(2)
   // console.log(await categories.update('Eletrônicos & Eletrodomésticos', 1))
+  // console.log(await products.findAll());
+  await products.create(['New product', 100])
+  console.log(await products.findAllByCategory(4));
 }
 
 test()
